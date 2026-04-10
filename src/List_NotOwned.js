20358d8bb066acc1495f3f715daeeb6015ba91b9
machi-fra <274831396+machi-fra@users.noreply.github.com>
2026-04-10 23:20:42 +0900

    初期登録

---
+++ src/List_NotOwned.js
@@ -0,0 +1,94 @@
+const LIST_NOT_OWNED = [
+    {
+        "player_jp_name": "ゴリラ",
+        "hero_name": "TANK_009"
+    },
+    {
+        "player_jp_name": "ゴリラ",
+        "hero_name": "TANK_006"
+    },
+    {
+        "player_jp_name": "ゴリラ",
+        "hero_name": "SPRINTER_007"
+    },
+    {
+        "player_jp_name": "ゴリラ",
+        "hero_name": "SPRINTER_013"
+    },
+    {
+        "player_jp_name": "ほしこ",
+        "hero_name": "SPRINTER_007"
+    },
+    {
+        "player_jp_name": "ほしこ",
+        "hero_name": "SPRINTER_013"
+    },
+    {
+        "player_jp_name": "ほしこ",
+        "hero_name": "SPRINTER_020"
+    },
+    {
+        "player_jp_name": "ほしこ",
+        "hero_name": "SPRINTER_021"
+    },
+    {
+        "player_jp_name": "ほしこ",
+        "hero_name": "SPRINTER_025"
+    },
+    {
+        "player_jp_name": "ほしこ",
+        "hero_name": "GUNNER_024"
+    },
+    {
+        "player_jp_name": "ほしこ",
+        "hero_name": "GUNNER_027"
+    },
+    {
+        "player_jp_name": "ほしこ",
+        "hero_name": "ATTACKER_029"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "ATTACKER_008"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "ATTACKER_017"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "GUNNER_011"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "TANK_005"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "TANK_007"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "TANK_009"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "TANK_010"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "TANK_015"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "SPRINTER_010"
+    },
+    {
+        "player_jp_name": "らいねるさん",
+        "hero_name": "SPRINTER_025"
+    },
+    {
+        "player_jp_name": "すゃさん",
+        "hero_name": "ATTACKER_023"
+    }
+];
---
+++ src/List_Stage.js
@@ -0,0 +1,24 @@
+// カラム構成: mode(対戦人数), name(ステージ名), jp_name(ステージ和名)
+// 対戦人数(mode): 3 (3v3用), 2 (2v2用)
+const LIST_STAGE = [
+    { mode: 2, name: 'stage_2_001', jp_name: '妖華帝都ケルパーズの散歩道' },
+    { mode: 3, name: 'stage_3_001', jp_name: 'でらクランクストリート' },
+    { mode: 3, name: 'stage_3_002', jp_name: '東西たかさん広場' },
+    { mode: 3, name: 'stage_3_003', jp_name: '立体交差のある風景' },
+    { mode: 3, name: 'stage_3_004', jp_name: '妖華帝都ケルパーズの散歩道' },
+    { mode: 3, name: 'stage_3_005', jp_name: 'って～！つっぺる工事現場' },
+    { mode: 3, name: 'stage_3_006', jp_name: 'おいでやす鳥居通り' },
+    { mode: 3, name: 'stage_3_007', jp_name: 'かけだせ！じっぱか城' },
+    { mode: 3, name: 'stage_3_008', jp_name: 'Castle of the Silver Feet' },
+    { mode: 3, name: 'stage_3_009', jp_name: 'Hello :-） irregular World' },
+    { mode: 3, name: 'stage_3_010', jp_name: 'おっかねぇ！氷焔神域ケイオスブルク' },
+    { mode: 3, name: 'stage_3_011', jp_name: 'けっこいスターパーク' },
+    { mode: 3, name: 'stage_3_012', jp_name: 'グレートウォール' },
+    { mode: 3, name: 'stage_3_013', jp_name: '光と闇のライブステージ' },
+    { mode: 3, name: 'stage_3_014', jp_name: 'ちゅら島リゾート' },
+    { mode: 3, name: 'stage_3_015', jp_name: '太鼓で祭りだドーン！' },
+    { mode: 3, name: 'stage_3_016', jp_name: 'どっひゃぁ〜！なっから遺跡' },
+    { mode: 3, name: 'stage_3_017', jp_name: 'マジきてる #夜光犯罪特区' },
+    { mode: 3, name: 'stage_3_018', jp_name: 'なまらモーモーパーク' },
+    { mode: 3, name: 'stage_3_019', jp_name: 'バリバリ！てげてげモーターウェイ' }
+];
